b29cddea696b490d67a9341c6fd019fa940eb237
Andres ZeroCross <ultimate.lancaster@gmail.com>
2024-06-11 17:13:02 +0700

    Added constants for macOS 15 support
    Added constants for macOS 15 support

--- CpuTscSync/kern_start.cpp
+++ CpuTscSync/kern_start.cpp
@@ -35,7 +35,7 @@ PluginConfiguration ADDPR(config) {
 	bootargBeta,
 	arrsize(bootargBeta),
 	KernelVersion::MountainLion,
-	KernelVersion::Sonoma,
+	KernelVersion::Sequoia,
 	[]() {
 		cpuf.init();
 	}
